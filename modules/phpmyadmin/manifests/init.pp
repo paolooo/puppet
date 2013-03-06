890faf16c4ec0c6c6b399a23d0af87584192f93b
Nino Paolo <o70vd@yahoo.com>
2013-03-06 23:41:10 +0800

    remove the old phpmyadmin and create a custom one. old one doesn't work

---
+++ modules/phpmyadmin/manifests/init.pp
@@ -0,0 +1,23 @@
+class phpmyadmin {
+  package { 'phpmyadmin':
+    ensure  => present,
+    require => [ Exec['apt-get update'], Package['php5', 'php5-mysql', 'apache2'] ]
+  }
+
+  file { '/etc/apache2/conf.d/phpmyadmin.conf':
+    ensure  => link,
+    target  => '/etc/phpmyadmin/apache.conf',
+    require => Package['apache2'],
+    notify  => Service['apache2']
+  }
+
+  file { 'phpmyadmin_config':
+    path    => '/etc/phpmyadmin/config.inc.php',
+    content => template('phpmyadmin/config.inc/php'),
+    ensure  => file,
+    owner   => root,
+    group   => 0,
+    mode    => 0444,
+    require => Package['phpmyadmin']
+  }
+}
